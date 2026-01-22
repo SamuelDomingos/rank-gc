78dc188470b45bcdf5cf41446556b48c17f45548
SamuelDomingos <89481523+SamuelDomingos@users.noreply.github.com>
2026-01-22 15:31:22 -0300

    ARRUMADO HEADER 3

--- components/header.tsx
+++ components/header.tsx
@@ -123,7 +123,7 @@ export const Header = () => {
     grid-cols-1
     sm:grid-cols-2
     md:grid-cols-3
-    lg:grid-cols-4
+    lg:grid-cols-5
   "
         >
           {typesPoints.map((item) => (
